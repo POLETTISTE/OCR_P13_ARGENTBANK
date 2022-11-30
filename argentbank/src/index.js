59db9c8c1dada56880765249d55d8ef0dfe112c4
POLETTI Stéphane <polettistephane@msn.com>
2022-11-30 15:23:53 +0100

    clean react app origin tools / create component header / create page accueil

--- argentbank/src/index.js
+++ argentbank/src/index.js
@@ -2,5 +2,11 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import "../src/styles/main.scss"
 
+import Accueil from "./pages/Accueil"
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(
+  <React.StrictMode>
+    <Accueil />
+  </React.StrictMode>
+)
---
+++ argentbank/src/pages/Accueil.jsx
@@ -0,0 +1,7 @@
+import Header from "../components/Header"
+
+const Accueil = () => {
+  return <Header />
+}
+
+export default Accueil
