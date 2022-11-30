a2ebf3a638be5b31c1f76bb1eacdfc98b241c436
POLETTI Stéphane <polettistephane@msn.com>
2022-11-30 16:57:11 +0100

    add css

--- argentbank/src/components/Header/index.jsx
+++ argentbank/src/components/Header/index.jsx
@@ -1,11 +1,26 @@
 import "./style.scss"
 import logo from "../../img/argentBankLogo.png"
+import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
-    <header>
-      <img src={logo} alt="logo argentbank" />
-    </header>
+    <nav className="main-nav">
+      <Link to={`/`} className="main-nav-logo">
+        <img
+          src={logo}
+          alt="Argent Bank Logo"
+          className="main-nav-logo-image"
+        />
+
+        <h1 className="sr-only">Argent Bank</h1>
+      </Link>
+      <div>
+        <a class="main-nav-item" href="./sign-in.html">
+          <i class="fa fa-user-circle"></i>
+          Sign In
+        </a>
+      </div>
+    </nav>
   )
 }
 
--- argentbank/src/index.js
+++ argentbank/src/index.js
@@ -2,11 +2,19 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import "../src/styles/main.scss"
 
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 import Accueil from "./pages/Accueil"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
-    <Accueil />
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Accueil />}></Route>
+        {/* <Route path="/user/:userId" element={<User />}></Route> */}
+        {/* <Route path="*" element={<Error />}></Route> */}
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
