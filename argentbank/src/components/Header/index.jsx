f001a404714f127c71ce34f25dc051a62bba6993
POLETTI Stéphane <polettistephane@msn.com>
2022-12-14 16:31:58 +0100

    create modal form before validation axios

--- argentbank/src/components/Header/index.jsx
+++ argentbank/src/components/Header/index.jsx
@@ -1,10 +1,11 @@
 import "./style.scss"
 import axios from "axios"
-import { useDispatch, useSelector } from "react-redux"
-import { useEffect } from "react"
+import { useSelector } from "react-redux"
+import { useState } from "react"
 
 const Header = () => {
-  const dispatch = useDispatch()
+  // const dispatch = useDispatch()
+  const [userFirstName, setUserFirstName] = useState("")
 
   const firstName = useSelector((state) => state.login.firstName)
 
@@ -16,27 +17,29 @@ const Header = () => {
     console.log(modal)
   }
 
-  const editNameValidation = () => {
+  const editNameValidation = (e) => {
+    e.preventDefault()
+    console.log("validation changements")
     const token = window.localStorage.getItem("token")
-    axios
-      .put(
-        "http://localhost:3001/api/v1/user/profile",
+    //   axios
+    //     .put(
+    //       "http://localhost:3001/api/v1/user/profile",
 
-        {
-          //enregistre prenom
-        },
-        {
-          headers: {
-            accept: "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      )
-      .then((res) => {
-        // dispatch(updateFirstName(res.data.body.firstName))
-        console.log(res)
-      })
-      .catch((error) => console.log("erreur dans l'API page User", error))
+    //       {
+    //         //enregistre prenom
+    //       },
+    //       {
+    //         headers: {
+    //           accept: "application/json",
+    //           Authorization: `Bearer ${token}`,
+    //         },
+    //       }
+    //     )
+    //     .then((res) => {
+    //       // dispatch(updateFirstName(res.data.body.firstName))
+    //       console.log(res)
+    //     })
+    //     .catch((error) => console.log("erreur dans l'API page User", error))
   }
 
   return (
@@ -49,7 +52,26 @@ const Header = () => {
       <button className="edit-button" onClick={editName}>
         Edit Name
       </button>
-      <div className="modal hide">ICI EST LA MODALE POUR MODIFIER</div>
+      <div className="modal hide">
+        <form>
+          <label htmlFor="username">
+            Username
+            <input
+              id="username"
+              onChange={(e) => setUserFirstName(e.target.value)}
+              value=""
+            />
+          </label>
+          <button
+            type="submit"
+            value="submit"
+            className=""
+            onClick={editNameValidation}
+          >
+            ok
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
