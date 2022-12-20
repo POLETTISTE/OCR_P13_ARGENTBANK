96d1cad312707e20dbba196af083c0a7d7353d7c
POLETTI Stéphane <polettistephane@msn.com>
2022-12-20 11:28:55 +0100

    Protected route in index.js for user's page

---
+++ argentbank/src/components/App/index.jsx
@@ -0,0 +1,44 @@
+import "./style.scss"
+import Nav from "../Nav/index"
+import Footer from "../Footer/index"
+import PageAccueil from "../../pages/PageAccueil"
+import PageSignIn from "../../pages/PageSignIn"
+import PageUser from "../../pages/PageUser"
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { useSelector } from "react-redux"
+
+const App = () => {
+  const isConnected = useSelector((state) => state.login.isConnected)
+
+  const ProtectedRoute = ({ children }) => {
+    if (!isConnected) {
+      return <Navigate to="/" replace />
+    }
+    return children
+  }
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route exact path="/" element={<PageAccueil />}></Route>
+          <Route exact path="/index" element={<PageAccueil />}></Route>
+          <Route exact path="/sign-in" element={<PageSignIn />}></Route>
+          <Route
+            exact
+            path="/user"
+            element={
+              <ProtectedRoute>
+                <PageUser />
+              </ProtectedRoute>
+            }
+          ></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
--- argentbank/src/index.js
+++ argentbank/src/index.js
@@ -4,28 +4,14 @@ import ReactDOM from "react-dom/client"
 import { Provider } from "react-redux"
 import store from "./store"
 
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-
-import Nav from "./components/Nav"
-import Footer from "./components/Footer"
-import PageAccueil from "./pages/PageAccueil"
-import PageSignIn from "./pages/PageSignIn"
-import PageUser from "./pages/PageUser"
+import App from "./components/App"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <Nav />
-        <Routes>
-          <Route exact path="/" element={<PageAccueil />}></Route>
-          <Route exact path="/index" element={<PageAccueil />}></Route>
-          <Route exact path="/sign-in" element={<PageSignIn />}></Route>
-          <Route exact path="/user" element={<PageUser />}></Route>
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <App />
     </Provider>
   </React.StrictMode>
 )
